a37da183e06bb5fd2d41e7f572e8cdb6b0741f4c
Linerre <leon.errelin@outlook.com>
2022-11-24 01:19:47 +0800

    Init the repo

---
+++ lecture01/hello_world.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+/**
+   1. In java, all code must be part of a class.
+   2. Classes are defined with "public class CLASSNAME"
+   3. Use { } to delineate the beginning and ending of things.
+*/
